17047aef34f403bf56cb0f8d43c8e7845ebef505
Amadhya <eshuanand07@gmail.com>
2019-08-31 23:30:09 +0530

    Register API done frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
+import logger from 'redux-logger'
 import thunk from 'redux-thunk';
 
 
@@ -9,7 +10,7 @@ import Register from "./pages/Register";
 import rootReducer from "./reducer";
 
 const middlewares = [thunk];
-const store = createStore(rootReducer,{},applyMiddleware(...middlewares));
+const store = createStore(rootReducer,{},applyMiddleware(logger,...middlewares));
 
 function App() {
   return (
