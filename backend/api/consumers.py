5f99a2d612084a9d8d6e21508192dc9e04ac6ccc
Mohit Kumar <mohit.verma741@gmail.com>
2019-09-02 00:11:49 +0530

    Added api endpoints
    * Added api endpoints
    
    * Added .gitignore file
    
    * mohit: Changed consumer methods to asynchronous
    
    * mohit: Changed consumer methods to asynchronous
    
    * mohit : Updated requirements.txt and readme
    
    * mohit : new connection and disconnect notify
    
    * mohit : new connection and disconnect notify

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -9,20 +9,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = 'chat_%s' % self.room_name
         # print(alias_set)
         # print(encrypted_map)
+        
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
+        active = list(alias_set)
+        await self.send(text_data=json.dumps({
+            'active':active,
+            'type':'--online--',
+        }))
 
 
     async def disconnect(self, close_code):
+        user = encrypted_map[self.current]
         alias_set.remove(encrypted_map[self.current])
         encrypted_map.pop(self.current,None)
         # print(alias_set)
         # print(encrypted_map)
+
+        # Notify all members that this user has left
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'disconnect_notify',
+                'alias': user,
+            }
+        )
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -35,6 +50,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
         alias = text_data_json['alias']
         if message == '--check--':
             self.current = alias
+            # Notify all users that this user has joined
+            await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'connect_notify',
+                'alias': encrypted_map[self.current],
+            }
+            )
             return
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -45,6 +68,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
     
+    # Handler for normal message
     async def chat_message(self, event):
         message = event['message']
         alias = event['alias']
@@ -53,4 +77,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message,
             'alias':alias,
+            'type':'--msg--',
         }))
+
+
+    # Handler for disconnect notification
+    async def disconnect_notify(self,event):
+        alias = event['alias']
+        await self.send(text_data=json.dumps({
+            'alias':alias,
+            'type':'--offline--',
+        }))
+
+
+    # Handler for new connection notification
+    async def connect_notify(self,event):
+        alias = event['alias']
+        await self.send(text_data=json.dumps({
+            'alias':alias,
+            'type':'--new--',
+        }))
+
